5af001f3db9819537932f6ff557ce42ed2c6baf3
Paul Patterson <35617787+RastaProgrammer@users.noreply.github.com>
2019-05-04 20:02:05 -0400

    Service modification
    Service now monitors Internet connectivity and updates widget accordingly.

--- src/com/bushbungalo/services/IconUpdateService.java
+++ src/com/bushbungalo/services/IconUpdateService.java
@@ -1,6 +1,9 @@
 package com.bushbungalo.services;
 
+import com.bushbungalo.WeatherLionMain;
 import com.bushbungalo.WeatherLionWidget;
+import com.bushbungalo.WeatherLionWidget.WidgetUpdateService;
+import com.bushbungalo.utils.UtilityMethod;
 
 /**
  * @author Paul O. Patterson
@@ -12,6 +15,11 @@ import com.bushbungalo.WeatherLionWidget;
  * <b style="margin-left:-40px">Date Created:</b>
  * <br />
  * 10/12/18
+ * <br />
+ * <b style="margin-left:-40px">Updates:</b><br />
+ * <ul>
+ * 		<li>05/04/19 - Service now monitors Internet connectivity and updates widget accordingly.</li>
+ * </ul>
  */
 public class IconUpdateService implements Runnable
 {
@@ -25,14 +33,41 @@ public class IconUpdateService implements Runnable
 		{
 			try
 			{				
-				Thread.sleep( sleepTime );				
+				Thread.sleep( sleepTime );
+				
+				// keep track of Internet connectivity
+				WeatherLionMain.connectedToInternet = UtilityMethod.hasInternetConnection();
+				
+				// If the not connected to the Internet, for a connection
+				if( !WeatherLionMain.connectedToInternet )
+				{
+					// if there was no previous Internet connection, check for a return in connectivity
+					// and refresh the widget
+					if( WeatherLionWidget.usingPreviousData && WeatherLionMain.connectedToInternet 
+							&& UtilityMethod.updatedRequired() )
+					{    			
+						// run the weather service
+						WeatherLionWidget.ws = new WidgetUpdateService( false );
+						WeatherLionWidget.ws.execute();
+					}// end of if block
+				}// end of if block
+				
+				if( WeatherLionMain.connectedToInternet )
+				{
+					WeatherLionWidget.lblOffline.setVisible( false );
+				}// end of if block
+				else 
+				{
+					WeatherLionWidget.lblOffline.setVisible( true );
+				}// end of else block
+				
 				WeatherLionWidget.checkAstronomy();								
 			}// end of try block 
 			catch ( InterruptedException e ) 
 			{
-				// do nothing
+				UtilityMethod.logMessage( "severe", e.getMessage(), "IconUpdateService::run" );				
 			}// end of catch block
 		}// end of while block
-	}
+	}// end of method run
 
 }// end of class IconUpdateService
